c1441f873f9f22f49303148afae4ec580459abc5
MarcoPC <marcomatute@unah.hn>
2019-12-17 22:23:41 -0600

    Service Worker Gatsby Notifications

--- src/utils/sw.js
+++ src/utils/sw.js
@@ -1,13 +1,15 @@
 // show a notification after 15 seconds (the notification
 // permission must be granted first)
 setTimeout(() => {
-    self.registration.showNotification("Hello, world!")
-    console.log("entro a las notificaciones")
+    self.registration.showNotification("New prices in Diunsa Honduras")
+    console.log("New Prices Notification")
 }, 15000)
 
 // register a custom navigation route
 const customRoute = new workbox.routing.NavigationRoute(({ event }) => {
-    console.log("Entro al otro metodo de las notificaciones")
+    console.log("Entro al Metodo de Navigation Route")
 // ...
 })
 workbox.routing.registerRoute(customRoute)
+
+//https://developers.google.com/web/fundamentals/codelabs/push-notifications
